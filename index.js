41fa60957fc9cd3089f41ff534704e67a9ab5452
Maksim <i@mfedyanin.ru>
2025-03-04 14:52:51 +0300

    a lot of methods

--- index.js
+++ index.js
@@ -55,26 +55,26 @@ async function getProducts() {
   const items = [];
 
   try {
-    await page.goto('https://www.uniqlo.com/th/en/spl/ranking/men');
+    await page.goto('https://www.uniqlo.com/th/en/spl/ranking/men', {timeout: 10 * 1000});
 
     await page.waitForSelector('.product-tile', {timeout: 10 * 1000});
 
-    const products = await page.locator('.product-tile').all();
+    const products = await page.locator('.product-tile').all({timeout: 2 * 1000});
 
     for (let product of products) {
       const parent = product.locator('xpath=..');
       const title = await product.locator('.product-tile-product-description').innerText({timeout: 2 * 1000});
-      const priceWrapper = await product.locator('.fr-price-currency');
+      const priceWrapper = await product.locator('.fr-price-currency', {timeout: 2 * 1000});
       const currency = await priceWrapper.locator('.fr-price-currency-margin').innerText({timeout: 2 * 1000});
       const price = await priceWrapper.locator('span').last().innerText({timeout: 2 * 1000});
       const imageWrapper = product.locator('.ec-renewal-image-wrapper');
       const image = imageWrapper.locator('img');
-      const imageSrc = await image.getAttribute('src');
-      const link = await parent.getAttribute('href');
+      const imageSrc = await image.getAttribute('src', {timeout: 2 * 1000});
+      const link = await parent.getAttribute('href', {timeout: 2 * 1000});
       const colorsWrapper = product.locator('.chip-wrapper');
       const colors = colorsWrapper.getByRole('listitem');
       const color = colors.first();
-      const colorStyle = await color.getAttribute('style') || '';
+      const colorStyle = await color.getAttribute('style', {timeout: 2 * 1000}) || '';
       const colorOption = colorStyle.split('goods_')[1].split('_')[0];
 
       records.push({
@@ -93,23 +93,23 @@ async function getProducts() {
 
         const record = records[a]
 
-        await page.goto(`https://www.uniqlo.com${record.link}`)
+        await page.goto(`https://www.uniqlo.com${record.link}`, {timeout: 10 * 1000})
 
         await page.waitForSelector("[name='product-color-picker']", {timeout: 10 * 1000})
 
-        const colors = await page.locator("[name='product-color-picker']").all()
-        const sizes = await page.locator("[name='product-size-picker']").all()
+        const colors = await page.locator("[name='product-color-picker']").all({timeout: 2 * 1000})
+        const sizes = await page.locator("[name='product-size-picker']").all({timeout: 2 * 1000})
         const colorOptions = []
         const sizeOptions = []
         const inStock = await page.locator('.fr-text-annotation.weak').isVisible({timeout: 2 * 1000})
 
         for (let color of colors) {
-          const value = await color.getAttribute('value')
+          const value = await color.getAttribute('value', {timeout: 2 * 1000})
 
           colorOptions.push(`COL${value}`)
         }
         for (let size of sizes) {
-          const value = await size.getAttribute('value')
+          const value = await size.getAttribute('value', {timeout: 2 * 1000})
 
           sizeOptions.push(`SMA${value}`)
         }
